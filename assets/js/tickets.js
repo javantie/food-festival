c2ca5567c3665f0484ae8c9f6b140264432cae5c
Akeem C <javantieclarke@gmail.com>
2021-09-22 15:41:48 -0400

    separated the code into modules

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,16 +1,25 @@
-if (window.location.href.indexOf("tickets") > -1) {
+require("bootstrap");
+const createEl = require("./domMethods");
 
-    function purchaseTicket () {
 
-      modalEl.removeChild(modalBodyEl)
-      modalEl.removeChild(modalFooterEl)
+$(document).ready(function () {
+  if (window.location.href.indexOf("tickets") > -1) {
+    function purchaseTicket() {
+      modalEl.removeChild(modalBodyEl);
+      modalEl.removeChild(modalFooterEl);
 
-      modalEl.append(createEl("div", {class: "modal-body"},
-        createEl("h5", {class: "modal-title"}, 
-        `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
-        ),
-      ))
-      
+      modalEl.append(
+        createEl(
+          "div",
+          { class: "modal-body" },
+          createEl(
+            "h5",
+            { class: "modal-title" },
+            `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
+          )
+        )
+      );
     }
     purchaseBtn.addEventListener("click", purchaseTicket);
   }
+});
